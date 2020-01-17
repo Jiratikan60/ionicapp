1cb87318f008609a31923e91f3e698dfaf82b2d5
Jirat Bunme <jiatikan1993@gmail.com>
2020-01-17 19:11:49 +0700

    1.0.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,15 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+import { AboutPageModule } from "./about/about.module";
+import { ProfilePageModule } from "./profile/profile.module";
+import { ProductPageModule } from "./product/product.module";
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,AboutPageModule],
   providers: [
     StatusBar,
     SplashScreen,
---
+++ src/app/product/product.page.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { AlertController } from "@ionic/angular";
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.page.html',
+  styleUrls: ['./product.page.scss'],
+})
+export class ProductPage implements OnInit {
+
+  titleName:string
+productName:string
+buttonName:string
+buttoncancleName:string
+
+  constructor(public alertController:AlertController) { }
+
+  ngOnInit() {
+    this.titleName = 'รายการอาหาร';
+    this.productName = 'ราดหน้า';
+    this.buttonName = 'ราคา';
+    
+  }
+
+  async presentAlert(){
+    const alert = await this.alertController.create({
+      header: 'ราดหน้า',
+      subHeader: 'ราคาสินค้า',
+      message: 'ราคา 40 บาท',
+      buttons: ['OK']
+    })
+    await alert.present();
+  }
+  async cancelAlert(){
+    const alert = await this.alertController.create({
+      message: 'ยกเลิกรายการอาหาร',
+      buttons: ['OK']
+    })
+    await alert.present();
+}
+}
