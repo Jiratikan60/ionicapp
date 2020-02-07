514abce5c103ac1562178c1d1bebc4f5950ca1ba
Jirat Bunme <jiatikan1993@gmail.com>
2020-02-07 12:21:51 +0700

    1.0.2

--- src/app/about/about.page.ts
+++ src/app/about/about.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import axios from 'axios';
 @Component({
   selector: 'app-about',
   templateUrl: './about.page.html',
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutPage implements OnInit {
 
+  // การนำข้อมูลจาก Database มาใช้
+  name: string;
+  lastname: string;
+  detail: string;
+
   constructor() { }
 
   ngOnInit() {
+
+    // การนำข้อมูลจาก Database มาใช้
+    axios.get('http://localhost/ionicserver/get_profile.php').then((response) => {
+
+      // handle success
+      console.log(response.data);
+
+      this.name = response.data[1].name;
+      this.lastname = response.data[1].lastname;
+      this.detail = response.data[1].detail;
+
+    })
+      .catch((error) => {
+
+        // handel error
+        console.log(error);
+      })
+      .then(() => {
+        // alway executed
+      });
   }
 
 }
